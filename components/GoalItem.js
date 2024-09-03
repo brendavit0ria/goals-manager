2307de1f210e8a19bbd342571be8d31b514ec49b
Brenda Vitória <brendavitoria0602@gmail.com>
2024-09-03 14:32:58 -0300

    feat: utilizing props

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,24 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-function GoalItem() {
+function GoalItem(props) {
   return (
     <View style={styles.goalsItem}>
-      <Text style={styles.goalText}>{itemData.item.text}</Text>
+      <Text style={styles.goalText}>{props.text}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  goalsItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  goalText: {
+    color: "white",
+  },
+});
 
 export default GoalItem;
